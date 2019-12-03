fe2baa04c1835ad76402892df66f695c63ecb8ad
Bayon Forte <bayon@forteworks.com>
2019-12-03 08:25:45 -0500

    add particles-bg animation  library.

--- src/sideview.jsx
+++ src/sideview.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
- 
+import ParticlesBg from "particles-bg";
+
 
 function Sideview() {
   return (
     <div className="Sideview">
       Sideview Page
+      <ParticlesBg type="random" bg={true}/>
     </div>
   );
 }
